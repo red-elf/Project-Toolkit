68bcae0b14d372e5633d62bf5b0160d79c886a63
Milos Vasic <i@mvasic.ru>
2023-11-14 18:01:33 +0300

    Version set.

---
+++ Version/version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export VERSIONABLE_SNAPSHOT=true
+export VERSIONABLE_NAME="Project Toolkit"
+export VERSIONABLE_VERSION_PRIMARY="1"
+export VERSIONABLE_VERSION_SECONDARY="0"
+export VERSIONABLE_VERSION_PATCH="0"
+export VERSIONABLE_HOMEPAGE="https://github.com/red-elf/Project-Toolkit"
+export VERSIONABLE_DESCRIPTION="The set of modules required to develop and build your projects."
