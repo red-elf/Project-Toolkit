cb6e4d972497d466155c14a0c18acd0f8213a800
Milos Vasic <i@mvasic.ru>
2023-11-14 17:56:14 +0300

    Moving the responsibility to parent.

---
+++ Recipes/Installable/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: SUBMODULES_HOME not defined"
+  exit 1
+fi
+
+if [ -z "$REPO_TOOLKIT" ]; then
+
+  REPO_TOOLKIT="git@github.com:red-elf/Project-Toolkit.git"
+fi
+
+TARGET="$SUBMODULES_HOME"
+
+if cd "$TARGET" && ! git status; then
+
+  git clone "$REPO_TOOLKIT" . &&
+    git submodule init && git submodule update && \
+    echo "Project Roolkit has been installed:" && \
+    cd "$SUBMODULES_HOME" && ls -lF
+    
+else
+
+  echo "ERROR: Installation directory is invalid '$TARGET'"
+  exit 1  
+fi
+
