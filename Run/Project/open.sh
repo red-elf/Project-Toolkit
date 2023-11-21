b81335abb64e60c021977ecbf59f1eccf3d64101
Milos Vasic <i@mvasic.ru>
2023-11-21 16:15:31 +0300

    "TODO resolved."

--- Run/Project/open.sh
+++ Run/Project/open.sh
@@ -55,27 +55,4 @@ else
   echo "Using the default project IDE cmd: $IDE"
 fi
 
-# TODO: Move under the Software-Toolkit responsibility and make it reusable; Incorporate it into the iconify script
-# TODO: Change the SUBMODULES_HOME variable into: PROJECT_TOOLKIT_HOME; Leave SUBMODULES_HOME where it makes sense - work with submodules.
-#
-RUN_IN_TERMINAL() {
-
-  if [ -z "$1" ]; then
-
-    echo "ERROR: Command to run parameter is mandatory"
-    exit 1
-  fi
-
-  COMMAND_TO_RUN="$1"
-
-  if which mate-terminal >/dev/null 2>&1; then
-
-    mate-terminal --geometry=250x70 -e "$COMMAND_TO_RUN"
-    
-  else
-
-    gnome-terminal --geometry=250x70 -- /bin/bash -ic "source ~/.bashrc && $COMMAND_TO_RUN; read"
-  fi
-}
-
 sh "$SCRIPT_OPEN" "$IDE" "$PROJECT"
