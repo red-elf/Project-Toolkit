4ea27fb9d68ac2568b25e836ee6ec2e67c419b04
Milos Vasic <i@mvasic.ru>
2023-12-07 15:59:49 +0300

    "Extending settings setup."

---
+++ Recipes/VSCode/settings.user.json.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat << EOF
+{
+  "workbench.colorTheme": "Visual Studio 2017 Dark - C++",
+  "workbench.startupEditor": "none",
+  "update.showReleaseNotes": false,
+  "redhat.telemetry.enabled": false,
+  "go.toolsManagement.autoUpdate": true,
+  "cmake.useCMakePresets": "never",
+  "cmake.showOptionsMovedNotification": false
+}
+EOF
