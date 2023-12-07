ab8206617085f97f8d6183b56c0cee74322e31e5
Milos Vasic <i@mvasic.ru>
2023-12-07 19:44:10 +0300

    "Test formatting against the assets."

---
+++ Run/JSON/test_Formatting_Assets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# TODO: Incorporate this as the part of integration tests.
+
+HERE=$(pwd)
+ASSETS="$HERE/Assets/JSON/Formatting"
+SCRIPT_RUN="$HERE/Run/JSON/test_Formatting.sh"
+
+if ! test -e "$ASSETS"; then
+
+    echo "ERROR: Directory not found '$ASSETS'"
+    exit 1
+fi
+
+if ! test -e "$SCRIPT_RUN"; then
+
+    echo "ERROR: Script not found '$SCRIPT_RUN'"
+    exit 1
+fi
+
+sh "$SCRIPT_RUN" "$ASSETS"
