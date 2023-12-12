0335042d634c87bc62703eaf4c19922f9cd0f61d
Milos Vasic <i@mvasic.ru>
2023-12-12 16:39:35 +0300

    Auto-commit

--- Recipes/SonarQube/installation_parameters_sonarqube.sh
+++ Recipes/SonarQube/installation_parameters_sonarqube.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
-# TODO: Obtain and configure the SonarQube installation for bringing it up
-#
+if [ -z "$SONARQUBE_PORT" ]; then
 
-echo "SonarQube installation recipe is not defined for the Project Toolkit"
-exit 1
+    echo "ERROR: Environment variable not set 'SONARQUBE_PORT'"
+    exit 1
+fi
 
-# if [ -z "$SONARQUBE_PROJECT" ]; then
+if [ -z "$SONARQUBE_ADMIN_USERNAME" ]; then
 
-#     echo "ERROR: Environment variable not set 'SONARQUBE_PROJECT'"
-#     exit 1
-# fi
+    echo "ERROR: Environment variable not set 'SONARQUBE_ADMIN_USERNAME'"
+    exit 1
+fi
 
-# if [ -z "$SONARQUBE_PORT" ]; then
+if [ -z "$SONARQUBE_ADMIN_PASSWORD" ]; then
 
-#     echo "ERROR: Environment variable not set 'SONARQUBE_PORT'"
-#     exit 1
-# fi
+    echo "ERROR: Environment variable not set 'SONARQUBE_ADMIN_PASSWORD'"
+    exit 1
+fi
 
-# if [ -z "$SONARQUBE_ADMIN_USERNAME" ]; then
+ADMIN_USER="$SONARQUBE_ADMIN_USERNAME"
+ADMIN_PASSWORD="$SONARQUBE_ADMIN_PASSWORD"
 
-#     echo "ERROR: Environment variable not set 'SONARQUBE_ADMIN_USERNAME'"
-#     exit 1
-# fi
+# shellcheck disable=SC2034
+SONARQUBE_NAME=$(hostname)
 
-# if [ -z "$SONARQUBE_ADMIN_PASSWORD" ]; then
+# shellcheck disable=SC2034
+DB_USER="db$ADMIN_USER"
 
-#     echo "ERROR: Environment variable not set 'SONARQUBE_ADMIN_PASSWORD'"
-#     exit 1
-# fi
-
-# ADMIN_USER="$SONARQUBE_ADMIN_USERNAME"
-# ADMIN_PASSWORD="$SONARQUBE_ADMIN_PASSWORD"
-
-# # shellcheck disable=SC2034
-# SONARQUBE_NAME="$SONARQUBE_PROJECT"
-
-# # shellcheck disable=SC2034
-# DB_USER="db$ADMIN_USER"
-
-# # shellcheck disable=SC2034
-# DB_PASSWORD="db$ADMIN_PASSWORD"
+# shellcheck disable=SC2034
+DB_PASSWORD="db$ADMIN_PASSWORD"
 
