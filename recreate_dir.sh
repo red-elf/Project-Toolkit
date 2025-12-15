1abdec196c3fec6e580099f2e3b62e9a97279df5
Milos Vasic <i@mvasic.ru>
2025-12-15 17:18:48 +0300

    Recreate directory.

---
+++ recreate_dir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+
+  echo "ERROR: Please provide the directory you want to recreate"
+  exit 1
+fi
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: SUBMODULES_HOME not available"
+  exit 1
+fi
+
+WHAT="$1"
+
+echo "Recreating: '$WHAT'" && \
+    rm -rf "$WHAT" && mkdir -p "$WHAT" && cd "$WHAT" && pwd
